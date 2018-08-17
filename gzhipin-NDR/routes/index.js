f0599969ce5b0a56afd3115050db69eb364b8ecf
DR-- <dr188888@163.com>
2018-08-17 22:46:56 +0800

    zz

--- gzhipin-NDR/routes/index.js
+++ gzhipin-NDR/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const md5 = require('blueimp-md5')
 const {UserModel} = require('../db/models')
+const {ChatModel} = require('../db/models')
 const filter = {password: 0, __v: 0} // 过滤掉查询时不需要的属性数据
 
 /* GET home page. */
@@ -106,7 +107,7 @@ router.post('/update', function (req, res) {
 })
 
 
-// 根据cookie获取对应的user
+// 4.根据cookie获取对应的user
 router.get('/user', function (req, res) {
   // 取出cookie中的userid
   const userid = req.cookies.userid
@@ -125,14 +126,62 @@ router.get('/user', function (req, res) {
   })
 })
 
-/*
-查看用户列表(指定类型的)
- */
+
+// 5.查看用户列表(指定类型的)
+
 router.get('/userlist',function(req, res){
   const { type } = req.query
   UserModel.find({type},filter, function(err,users){
     return res.json({code:0, data: users})
   })
 })
+
+//6.获取当前用户的聊天消息列表
+router.get('/msglist', function (req, res) {
+  // 获取cookie中的userid
+  const userid = req.cookies.userid
+  // 查询得到所有user文档数组
+  UserModel.find(function (err, userDocs) {
+    // 用对象存储所有user信息: key为user的_id, val为name和header组成的user对象
+    const users = {} // 对象容器
+    userDocs.forEach(doc => {
+      users[doc._id] = {username: doc.username, header: doc.header}
+    })
+    /*
+    查询userid相关的所有聊天信息
+     参数1: 查询条件
+     参数2: 过滤条件
+     参数3: 回调函数
+    */
+    ChatModel.find({'$or': [{from: userid}, {to: userid}]}, filter, function (err, chatMsgs) {
+      // 返回包含所有用户和当前用户相关的所有聊天消息的数据
+      res.send({code: 0, data: {users, chatMsgs}})
+    })
+  })
+})
+
+/*
+修改指定消息为已读
+ */
+router.post('/readmsg', function (req, res) {
+  // 得到请求中的from和to
+  const from = req.body.from
+  const to = req.cookies.userid
+  /*
+  更新数据库中的chat数据
+  参数1: 查询条件
+  参数2: 更新为指定的数据对象
+  参数3: 是否1次更新多条, 默认只更新一条
+  参数4: 更新完成的回调函数
+   */
+  ChatModel.update({from, to, read: false}, {read: true}, {multi: true}, function (err, doc) {
+    console.log('/readmsg', doc)
+    res.send({code: 0, data: doc.nModified}) // 更新的数量
+  })
+})
+
+
+
+
 
 module.exports = router;
